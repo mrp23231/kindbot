99ac4de4dc91a4f48c854b58a9003aa2f607f169
mrp23231 <akkgame158@gmail.com>
2025-07-05 14:22:50 +0300

    Update utils.py

--- utils.py
+++ utils.py
@@ -1,12 +1,14 @@
 import json
+import os
+
+DATA_FILE = "database.json"
 
 def load_data():
-    try:
-        with open("database.json", "r", encoding="utf-8") as f:
-            return json.load(f)
-    except:
+    if not os.path.exists(DATA_FILE):
         return {"stories": []}
+    with open(DATA_FILE, "r", encoding="utf-8") as f:
+        return json.load(f)
 
 def save_data(data):
-    with open("database.json", "w", encoding="utf-8") as f:
+    with open(DATA_FILE, "w", encoding="utf-8") as f:
         json.dump(data, f, ensure_ascii=False, indent=2)
