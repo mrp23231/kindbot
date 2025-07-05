a908bb83dbe167f77991ff771924bf8c021553bc
mrp23231 <akkgame158@gmail.com>
2025-07-05 14:19:44 +0300

    Create bot_init.py

---
+++ bot_init.py
@@ -0,0 +1,6 @@
+from aiogram import Bot, Dispatcher
+from aiogram.contrib.fsm_storage.memory import MemoryStorage
+from config import TOKEN
+
+bot = Bot(token=TOKEN)
+dp = Dispatcher(bot, storage=MemoryStorage())
